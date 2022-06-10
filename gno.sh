c87f45ca05db8aa7fe57e82561aa5bf0c823ab4c
cryptongithub <96590783+cryptongithub@users.noreply.github.com>
2022-06-10 04:37:18 +0200

    Update gno.sh

--- gno.sh
+++ gno.sh
@@ -9,7 +9,7 @@ fi
 . $HOME/.bashrc && . $HOME/.bash_profile
 
 curl -s https://raw.githubusercontent.com/cryptongithub/init/main/logo.sh | bash 
-echo -e '\e[40m\e[92mCrypton Academy is a unique cryptocurrency community. \nCommunity chat, early gems, calendar of events, Ambassador programs, nodes, testnets, personal assistant. \nJoin (TG): \e[95mt.me/CryptonLobbyBot\e[40m\e[92m.\e[0m\n'
+echo -e '\e[40m\e[92mCrypton Academy is a unique cryptocurrency community. \nCommunity chat, early gems, calendar of events, Ambassador programs, nodes, testnets, personal assistant. \nJoin (TG RU): \e[95mt.me/CryptonLobbyBot\e[40m\e[92m.\nOur links (RU, EN, ES): \e[95mlinktr.ee/Crypton_EN\e[40m\e[92m.\e[0m\n'
 
 function install_and_create {
     echo 'alias gnokey="cd $HOME/gno && ./build/gnokey"' >> $HOME/.bashrc
